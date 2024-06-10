0408468d40ebe52c9a9d188d4a8d6777d2027fce
erii i nya dua <164143834+Eritriharyanto@users.noreply.github.com>
2024-06-10 13:25:10 +0700

    Add files via upload

---
+++ conditionalST.py
@@ -0,0 +1,5 @@
+x=3
+if(x%2)==1:
+	print("bilangan ganjil")
+if x==3:
+	print("angka tiga")
---
+++ if-elif-else.py
@@ -0,0 +1,7 @@
+x=3
+if (float(x).is_integer())!=True:
+	print("bukan bilangan bulat")
+elif (x%2)==1:
+	print("bilabgan ganjil")
+else:
+	print("bilangan genap")
